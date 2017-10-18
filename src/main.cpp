8357348461ce1a9397ab68a77d429ae8986ccc0b
William Ho <willho@seas.upenn.edu>
2017-10-18 16:31:47 -0400

    added images

--- src/main.cpp
+++ src/main.cpp
@@ -138,8 +138,8 @@ bool init(const tinygltf::Scene & scene) {
         return false;
     }
 
-    width = 400;
-    height = 400;
+    width = 800;
+    height = 800;
     window = glfwCreateWindow(width, height, "CIS 565 Pathtracer", NULL, NULL);
     if (!window) {
         glfwTerminate();
